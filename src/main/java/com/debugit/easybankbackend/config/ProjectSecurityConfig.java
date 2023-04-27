64d9444ddc9f915e45eee64f0f5bb339b26036c1
Ewerton Carreira <ewertoncarreira@gmail.com>
2023-04-27 18:02:08 -0300

    Demo of registration of new user with Bcrypt password encoder

--- src/main/java/com/debugit/easybankbackend/config/ProjectSecurityConfig.java
+++ src/main/java/com/debugit/easybankbackend/config/ProjectSecurityConfig.java
@@ -3,6 +3,7 @@ package com.debugit.easybankbackend.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -22,50 +23,8 @@ public class ProjectSecurityConfig {
         return http.build();
     }
 
-
-//    @Bean
-//    public InMemoryUserDetailsManager userDetailsService() {
-//        // Approach 1 where we use withDefaultPasswordEncoder() method while creating the user details
-//        /*UserDetails admin = User.withDefaultPasswordEncoder()
-//                .username("admin")
-//                .password("12345")
-//                .authorities("admin")
-//                .build();
-//
-//        UserDetails user = User.withDefaultPasswordEncoder()
-//                .username("user")
-//                .password("12345")
-//                .authorities("read")
-//                .build();
-//         */
-//
-//        // Approach 2 where we use NoOpPasswordEncoder() method while creating the user details
-//        UserDetails admin = User.withUsername("admin")
-//                .password("12345")
-//                .authorities("admin")
-//                .build();
-//
-//        UserDetails user = User.withUsername("user")
-//                .password("12345")
-//                .authorities("read")
-//                .build();
-//
-//        return new InMemoryUserDetailsManager(admin, user);
-//    }
-
-//    @Bean
-//    public UserDetailsService userDetailsService(DataSource dataSource) {
-//        return new JdbcUserDetailsManager(dataSource);
-//    }
-
-    /**
-     * NoOpPasswordEncoder is not recommended for production usage.
-     * Use only for non-prod
-     *
-     * @return PasswordEncoder
-     */
     @Bean
     public PasswordEncoder passwordEncoder() {
-        return NoOpPasswordEncoder.getInstance();
+        return new BCryptPasswordEncoder();
     }
 }
