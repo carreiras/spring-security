b887f465dda91bfdbdb46589830c698c7d70bb35
Ewerton Carreira <ewertoncarreira@gmail.com>
2023-04-26 23:29:26 -0300

    Denying all the requests

--- src/main/java/com/debugit/easybankbackend/config/ProjectSecurityConfig.java
+++ src/main/java/com/debugit/easybankbackend/config/ProjectSecurityConfig.java
@@ -10,11 +10,27 @@ public class ProjectSecurityConfig {
 
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+
+        /**
+         * Below is the custom security configurations
+         */
+
+        /*
         http.authorizeHttpRequests()
                 .requestMatchers("/myAccount", "/myBalance", "/myLoans", "myCards").authenticated()
                 .requestMatchers("/notices", "/contact").permitAll()
                 .and().formLogin()
                 .and().httpBasic();
+         */
+
+        /**
+         * Configuration to deny all the requests
+         */
+
+        http.authorizeHttpRequests().anyRequest().denyAll()
+                .and().formLogin()
+                .and().httpBasic();
+
         return http.build();
     }
 }
