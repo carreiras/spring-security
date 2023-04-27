30fb459af959b9fef18ba9778a6f282df61044f2
Ewerton Carreira <ewertoncarreira@gmail.com>
2023-04-27 00:14:41 -0300

    Configuring users using InMemoryUserDetailsManager - Approach 2

--- src/main/java/com/debugit/easybankbackend/config/ProjectSecurityConfig.java
+++ src/main/java/com/debugit/easybankbackend/config/ProjectSecurityConfig.java
@@ -5,6 +5,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -26,8 +28,7 @@ public class ProjectSecurityConfig {
     @Bean
     public InMemoryUserDetailsManager userDetailsService() {
         // Approach 1 where we use withDefaultPasswordEncoder() method while creating the user details
-
-        UserDetails admin = User.withDefaultPasswordEncoder()
+        /*UserDetails admin = User.withDefaultPasswordEncoder()
                 .username("admin")
                 .password("12345")
                 .authorities("admin")
@@ -38,7 +39,30 @@ public class ProjectSecurityConfig {
                 .password("12345")
                 .authorities("read")
                 .build();
+         */
+
+        // Approach 2 where we use NoOpPasswordEncoder() method while creating the user details
+        UserDetails admin = User.withUsername("admin")
+                .password("12345")
+                .authorities("admin")
+                .build();
+
+        UserDetails user = User.withUsername("user")
+                .password("12345")
+                .authorities("read")
+                .build();
 
         return new InMemoryUserDetailsManager(admin, user);
+    }
+
+    /**
+     * NoOpPasswordEncoder is not recommended for production usage.
+     * Use only for non-prod
+     *
+     * @return PasswordEncoder
+     */
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
     }
 }
