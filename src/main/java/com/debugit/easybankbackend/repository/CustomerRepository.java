089cb43d8a323f70b7b900773db7d740c4420e7c
Ewerton Carreira <ewertoncarreira@gmail.com>
2023-04-27 03:49:11 -0300

    Creating JPA Entity and repository classes for new table

---
+++ src/main/java/com/debugit/easybankbackend/repository/CustomerRepository.java
@@ -0,0 +1,13 @@
+package com.debugit.easybankbackend.repository;
+
+import com.debugit.easybankbackend.model.Customer;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface CustomerRepository extends CrudRepository<Customer, Long> {
+
+    List<Customer> findByEmail(String email);
+}
